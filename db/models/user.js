cc86b74157339e3a5e290d1588d3fc1c24e1b620
darren <dkong1321@gmail.com>
2022-04-11 15:42:33 -0700

    complete user registration

--- db/models/user.js
+++ db/models/user.js
@@ -1,21 +1,19 @@
 "use strict";
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    "User",
-    {
-      firstName: DataTypes.STRING,
-      lastName: DataTypes.STRING,
-      email: DataTypes.STRING,
-      hashedPassword: DataTypes.STRING,
-      phoneNumber: DataTypes.INTEGER,
-      occupation: DataTypes.STRING,
-      bio: DataTypes.TEXT,
-    },
-    {}
-  );
-  User.associate = function (models) {
-    // associations can be defined here
-    User.hasMany(models.Project, { foreignKey: "userId" });
-  };
-  return User;
+    const User = sequelize.define(
+        "User", {
+            firstName: DataTypes.STRING,
+            lastName: DataTypes.STRING,
+            email: DataTypes.STRING,
+            hashedPassword: DataTypes.STRING.BINARY,
+            phoneNumber: DataTypes.INTEGER,
+            occupation: DataTypes.STRING,
+            bio: DataTypes.TEXT,
+        }, {}
+    );
+    User.associate = function(models) {
+        // associations can be defined here
+        User.hasMany(models.Project, { foreignKey: "userId" });
+    };
+    return User;
 };
