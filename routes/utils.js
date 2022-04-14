76751126f25a0117998232a954f1e565a5eb14ac
Chris Threadgill <christhreadgill@live.com>
2022-04-14 17:53:17 -0500

    Fixed error with requiring user auth to utils.js for sign-up functionality

--- routes/utils.js
+++ routes/utils.js
@@ -1,3 +1,4 @@
+const db = require("../db/models");
 const csrf = require("csurf");
 
 const csrfProtection = csrf({ cookie: true });
@@ -5,12 +6,12 @@ const csrfProtection = csrf({ cookie: true });
 const { check, validationResult } = require("express-validator");
 
 const asyncHandler = (handler) => {
-    return (req, res, next) => {
-        return handler(req, res, next).catch(next);
-    };
+  return (req, res, next) => {
+    return handler(req, res, next).catch(next);
+  };
 };
 
-const contentTypeJson = { "Content-Type": "application/json" }
+const contentTypeJson = { "Content-Type": "application/json" };
 const emailReg = /^[^\s@]+@\w+\.[A-z]{2,3}$/;
 
 const lowerCase = /^(?=.*[a-z])/;
@@ -20,84 +21,84 @@ const alphaNumeric = /(?=.*[!@#$%^&*])/;
 const eightCharacters = /(?=.{8,})/;
 
 const userValidators = [
-    check("firstName")
+  check("firstName")
     .exists({ checkFalsy: true })
     .withMessage("Please provide a first name")
     .isLength({ max: 50 })
     .withMessage("First name cannot be more than 50 characters long"),
-    check("lastName")
+  check("lastName")
     .exists({ checkFalsy: true })
     .withMessage("Please provide a last name")
     .isLength({ max: 50 })
     .withMessage("Last name cannot be more than 50 characters long"),
-    check("email")
+  check("email")
     .exists({ checkFalsy: true })
     .withMessage("Please provide an email")
     .isLength({ max: 255 })
     .withMessage("email cannot be more than 255 characters long")
     .custom((value) => {
-        return db.User.findOne({ where: { email: value } }).then((user) => {
-            if (user) {
-                return Promise.reject(
-                    "The provided Email Address is already in use by another account"
-                );
-            }
-        });
+      return db.User.findOne({ where: { email: value } }).then((user) => {
+        if (user) {
+          return Promise.reject(
+            "The provided Email Address is already in use by another account"
+          );
+        }
+      });
     }),
-    check("hashedPassword")
+  check("hashedPassword")
     .exists({ checkFalsy: true })
     .withMessage("Please Provide a password")
     .isLength({ max: 255 })
     .withMessage("password must be less than 255 characters")
     .matches(lowerCase)
     .withMessage(
-        "Please input a password with at least one lower case character"
+      "Please input a password with at least one lower case character"
     )
     .matches(upperCase)
     .withMessage(
-        "Please input a password with at least one upper case character"
+      "Please input a password with at least one upper case character"
     )
     .matches(oneNumeric)
     .withMessage("Please input a password with at least one numeric character")
     .matches(alphaNumeric)
     .withMessage(
-        "Please input a password with at least one alpha numeric character"
+      "Please input a password with at least one alpha numeric character"
     )
     .matches(eightCharacters)
     .withMessage("Please input a password at least eight characters long"),
 
-    check("confirmPassword")
+  check("confirmPassword")
     .exists({ checkFalsy: true })
     .withMessage("Please provide a value for Confirm Password")
     .isLength({ max: 255 })
     .withMessage("Confirm Password must not be more than 255 characters long")
     .custom((value, { req }) => {
-        if (value !== req.body.hashedPassword) {
-            throw new Error("Confirm Password does not match Password");
-        }
-        return true;
+      if (value !== req.body.hashedPassword) {
+        throw new Error("Confirm Password does not match Password");
+      }
+      return true;
     }),
-    check("phoneNumber")
+  check("phoneNumber")
     .isLength({ max: 10 })
     .withMessage("Please input phone number with at most 10 digits"),
-    check("occupation")
+  check("occupation")
     .isLength({ max: 255 })
     .withMessage("Max character limit is 255 characters"),
 ];
 
 const loginValidators = [
-    check("email")
+  check("email")
     .exists({ checkFalsy: true })
     .withMessage("Please provide your login email address."),
-    check("hashedPassword")
+  check("hashedPassword")
     .exists({ checkFalsy: true })
     .withMessage("Don't forget your password!"),
 ];
 
 module.exports = {
-    csrfProtection,
-    asyncHandler,
-    userValidators,
-    loginValidators,
-    contentTypeJson,
+  csrfProtection,
+  asyncHandler,
+  userValidators,
+  loginValidators,
+  contentTypeJson,
 };
