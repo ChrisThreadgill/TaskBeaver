b886fd8900c302944eb10e24c1a4658bbe2e549e
darren <dkong1321@gmail.com>
2022-04-14 15:19:40 -0700

    issue with reigster

--- public/javascripts/listPopulate.js
+++ public/javascripts/listPopulate.js
@@ -1,44 +1,43 @@
 window.addEventListener("load", (e) => {
-  console.log("hello from list populate project script");
+    console.log("hello from list populate project script");
 });
 
 const projectLinks = document.querySelectorAll(".project__link");
-
 const projects = Array.from(projectLinks);
 const hiddenForm = document.querySelectorAll(".hidden__edit__form");
 const hiddenForms = Array.from(hiddenForm);
 
 for (let i = 0; i < projects.length; i++) {
-  // console.log(projects[i]);
-  const currentProject = projects[i];
 
-  const projectId = currentProject.id.split("__")[2];
-  const projectHeader = document.getElementById(
-    `project__header__${projectId}`
-  );
-  // console.log(projectHeader);
-  currentProject.addEventListener("click", async (e) => {
-    e.preventDefault();
-    const unhiddenHeader = document.querySelector(".project__header__unhidden");
-    const activeHiddenForm = document.getElementById("hidden__form__active");
+    const currentProject = projects[i];
+    console.log(currentProject)
+    const projectId = currentProject.id.split("__")[3];
+    const projectHeader = document.getElementById(
+        `project__header__${projectId}`
+    );
 
-    if (activeHiddenForm) {
-      console.log(activeHiddenForm);
-      activeHiddenForm.classList.toggle("hidden");
-      activeHiddenForm.removeAttribute("id");
-      console.log(activeHiddenForm);
-    }
+    currentProject.addEventListener("click", async(e) => {
+        e.preventDefault();
+        console.log("working")
+        const unhiddenHeader = document.querySelector(".project__header__unhidden");
+        const activeHiddenForm = document.getElementById("hidden__form__active");
 
-    if (unhiddenHeader) {
-      console.log("working");
-      // console.log(unhiddenHeader);
-      unhiddenHeader.removeAttribute("class");
-      unhiddenHeader.setAttribute("class", "hidden");
-      // console.log(unhiddenHeader);
-    }
-    projectHeader.removeAttribute("class");
-    projectHeader.setAttribute("class", "project__header__unhidden");
-    // projectHeader.setAttribute("class", "hidden");
-    // console.log(projectHeader);
-  });
+        if (activeHiddenForm) {
+            console.log(activeHiddenForm);
+            activeHiddenForm.classList.toggle("hidden");
+            activeHiddenForm.removeAttribute("id");
+            console.log(activeHiddenForm);
+        }
+
+        if (unhiddenHeader) {
+            console.log("working");
+            unhiddenHeader.removeAttribute("class");
+            unhiddenHeader.setAttribute("class", "hidden");
+
+        }
+        projectHeader.removeAttribute("class");
+        projectHeader.setAttribute("class", "project__header__unhidden");
+        // projectHeader.setAttribute("class", "hidden");
+        // console.log(projectHeader);
+    });
 }
--- routes/modal.js
+++ routes/modal.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const {
-  csrfProtection,
-  asyncHandler,
-  userValidators,
-  loginValidators,
+    csrfProtection,
+    asyncHandler,
+    userValidators,
+    loginValidators,
 } = require("./utils.js");
 const db = require("../db/models");
 const { loginUser, logoutUser } = require("../auth");
@@ -13,147 +13,151 @@ const bcrypt = require("bcryptjs");
 
 /* GET home page. */
 router.get(
-  "/",
-  csrfProtection,
-  asyncHandler(async (req, res, next) => {
-    let user = {};
-    let projects = {};
-    let tasks = { taskTitle: "Make a new Task" };
-    let projectDisplay = { projectName: "Project Title" };
-    let navBarTasks = { inProgress: 0, dueToday: 0, completed: 0 };
+    "/",
+    csrfProtection,
+    asyncHandler(async(req, res, next) => {
+        let user = {};
+        let projects = {};
+        let tasks = { taskTitle: "Make a new Task" };
+        let projectDisplay = { projectName: "Project Title" };
+        let navBarTasks = { inProgress: 0, dueToday: 0, completed: 0 };
 
-    if (req.session.auth) {
-      const userId = req.session.auth.userId;
-      user = await db.User.findByPk(userId);
-      projects = await db.Project.findAll({
-        where: {
-          userId,
-        },
-      });
-      const projectId = projects[0].id;
-      projectDisplay = projects[0];
-      tasks = await db.Task.findAll({ where: { projectId } });
-    }
-
-    res.render("modal", {
-      title: "Test Modal",
-      user,
-      projects,
-      projectDisplay,
-      tasks,
-      navBarTasks,
-      csrfToken: req.csrfToken(),
-    });
-  })
-);
-
-router.post(
-  "/sign-up",
-  csrfProtection,
-  userValidators,
-  asyncHandler(async (req, res, next) => {
-    let projects = {};
-    let tasks = { taskTitle: "Make a new Task" };
-    let projectDisplay = { projectName: "Project Title" };
-    let navBarTasks = { inProgress: 0, dueToday: 0, completed: 0 };
-    const {
-      firstName,
-      lastName,
-      email,
-      hashedPassword,
-      phoneNumber,
-      occupation,
-      bio,
-    } = req.body;
-
-    const user = await db.User.build({
-      firstName,
-      lastName,
-      email,
-      hashedPassword,
-      phoneNumber,
-      occupation,
-      bio,
-    });
-
-    const errors = validationResult(req);
-
-    if (errors.isEmpty()) {
-      const salt = parseInt(process.env.SALT, 10);
-      const hash = await bcrypt.hash(hashedPassword, salt);
-      user.hashedPassword = hash;
-      await user.save();
-      loginUser(req, res, user);
-      res.redirect("/");
-    } else {
-      const errorArray = errors.array().map((error) => error.msg);
-      res.render("modal", {
-        title: "Sign-Up",
-        errorArray,
-        user,
-        projects,
-        projectDisplay,
-        tasks,
-        navBarTasks,
-        csrfToken: req.csrfToken(),
-      });
-    }
-  })
-);
-
-router.post(
-  "/login",
-  csrfProtection,
-  loginValidators,
-  asyncHandler(async (req, res, next) => {
-    let user = {};
-    let projects = {};
-    let tasks = { taskTitle: "Make a new Task" };
-    let projectDisplay = { projectName: "Project Title" };
-    let navBarTasks = { inProgress: 0, dueToday: 0, completed: 0 };
-    const { email, hashedPassword } = req.body;
-    let errors = [];
-    const loginErrors = validationResult(req);
-    // console.log(req.body);
-
-    if (loginErrors.isEmpty()) {
-      const user = await db.User.findOne({ where: { email } });
-      if (user !== null) {
-        const passwordMatch = await bcrypt.compare(
-          hashedPassword,
-          user.hashedPassword.toString()
-        );
-        if (passwordMatch) {
-          loginUser(req, res, user);
-          req.session.save(() => res.redirect("/"));
-
-          return;
+        if (req.session.auth) {
+            const userId = req.session.auth.userId;
+            user = await db.User.findByPk(userId);
+            projects = await db.Project.findAll({
+                where: {
+                    userId,
+                },
+                include: db.Task,
+                order: [
+                    ["id", "ASC"]
+                ]
+            });
+            console.log("=========", projects[0].Tasks)
+            const projectId = projects[0].id;
+            projectDisplay = projects[0];
+            tasks = await db.Task.findAll({ where: { projectId } });
         }
-      }
-      errors.push("Login Failed");
-    } else {
-      errors = loginErrors.array().map((error) => error.msg);
-    }
-    res.render("modal", {
-      title: "Home",
-      email,
-      errors,
-      user,
-      projects,
-      projectDisplay,
-      tasks,
-      navBarTasks,
-      csrfToken: req.csrfToken(),
-    });
-  })
+        res.render("modal", {
+            title: "Test Modal",
+            user,
+            projects,
+            projectDisplay,
+            tasks,
+            navBarTasks,
+            csrfToken: req.csrfToken(),
+        });
+    })
 );
 
 router.post(
-  "/logout",
-  asyncHandler(async (req, res) => {
-    logoutUser(req, res);
-    req.session.save(() => res.redirect("/"));
-  })
+    "/sign-up",
+    csrfProtection,
+    userValidators,
+    asyncHandler(async(req, res, next) => {
+        let projects = {};
+        let tasks = { taskTitle: "Make a new Task" };
+        let projectDisplay = { projectName: "Project Title" };
+        let navBarTasks = { inProgress: 0, dueToday: 0, completed: 0 };
+        const {
+            firstName,
+            lastName,
+            email,
+            hashedPassword,
+            phoneNumber,
+            occupation,
+            bio,
+        } = req.body;
+
+        const user = await db.User.build({
+            firstName,
+            lastName,
+            email,
+            hashedPassword,
+            phoneNumber,
+            occupation,
+            bio,
+        });
+
+        const errors = validationResult(req);
+
+        if (errors.isEmpty()) {
+            const salt = parseInt(process.env.SALT, 10);
+            const hash = await bcrypt.hash(hashedPassword, salt);
+            user.hashedPassword = hash;
+            await user.save();
+            loginUser(req, res, user);
+            res.redirect("/");
+        } else {
+            const errorArray = errors.array().map((error) => error.msg);
+            res.render("modal", {
+                title: "Sign-Up",
+                errorArray,
+                user,
+                projects,
+                projectDisplay,
+                tasks,
+                navBarTasks,
+                csrfToken: req.csrfToken(),
+            });
+        }
+    })
+);
+
+router.post(
+    "/login",
+    csrfProtection,
+    loginValidators,
+    asyncHandler(async(req, res, next) => {
+        let user = {};
+        let projects = {};
+        let tasks = { taskTitle: "Make a new Task" };
+        let projectDisplay = { projectName: "Project Title" };
+        let navBarTasks = { inProgress: 0, dueToday: 0, completed: 0 };
+        const { email, hashedPassword } = req.body;
+        let errors = [];
+        const loginErrors = validationResult(req);
+        // console.log(req.body);
+
+        if (loginErrors.isEmpty()) {
+            const user = await db.User.findOne({ where: { email } });
+            if (user !== null) {
+                const passwordMatch = await bcrypt.compare(
+                    hashedPassword,
+                    user.hashedPassword.toString()
+                );
+                if (passwordMatch) {
+                    loginUser(req, res, user);
+                    req.session.save(() => res.redirect("/"));
+
+                    return;
+                }
+            }
+            errors.push("Login Failed");
+        } else {
+            errors = loginErrors.array().map((error) => error.msg);
+        }
+        res.render("modal", {
+            title: "Home",
+            email,
+            errors,
+            user,
+            projects,
+            projectDisplay,
+            tasks,
+            navBarTasks,
+            csrfToken: req.csrfToken(),
+        });
+    })
+);
+
+router.post(
+    "/logout",
+    asyncHandler(async(req, res) => {
+        logoutUser(req, res);
+        req.session.save(() => res.redirect("/"));
+    })
 );
 
 module.exports = router;
