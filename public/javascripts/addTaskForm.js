1e199524d0fd01730063bffb5534a61fbbec2922
joshua <jrbauti1@asu.edu>
2022-04-14 12:14:08 -0700

    fixing add task javascript

--- public/javascripts/addTaskForm.js
+++ public/javascripts/addTaskForm.js
@@ -1,7 +1,11 @@
 const contentTypeJson = { "Content-Type": "application/json" }
 
 window.addEventListener("load", (event) => {
-    console.log("hello from javascript!")
+    console.log("This is the addTaskForm!")
+
+
+    console.log(document.querySelector('.add__task__id'))
+
 })
 
 
@@ -10,22 +14,39 @@ const info = { "projectName": "poop1", "description": "this is poop", "dueDate":
 
 
 // Will grab button for add task.
-const newTask = document.getElementById('confirm__Task__Button')
+const newTask = document.querySelector('.test__task__add')
+
+console.log(newTask)
+
+// Grab values from our inputs.
+const taskTitle = document.querySelector('#taskTitle')
+// const description =
+// const projectId =
+// const dueDate =
+// const tag =
+
+console.log(taskTitle.value)
 
 // Will listen for click
 newTask.addEventListener("click", async(e) => {
+    e.preventDefault();
 
-    const postTask = await fetch('/api/tasks', {
-        method: "post",
-        body: JSON.stringify(info),
-        headers: contentTypeJson
-    })
-    const project = await postTask.json()
-    const { projectName, id, } = project.project
-    const projectList = document.querySelector('.project__list')
-    const projectLink = document.createElement('div')
-    projectLink.innerHTML = `<a href=/projects/${id}> ${projectName}</a>`
-    projectLink.setAttribute('class', 'project__link')
-    projectLink.setAttribute('id', `project-id-${id}`)
-    projectList.appendChild(projectLink)
+    console.log('working')
+    console.log(taskTitle.value)
+
+    // const postTask = await fetch('/api/tasks', {
+    //     method: "post",
+    //     body: JSON.stringify(info),
+    //     headers: contentTypeJson
+    // })
+
+
+    // const project = await postTask.json()
+    // const { projectName, id, } = project.project
+    // const projectList = document.querySelector('.project__list')
+    // const projectLink = document.createElement('div')
+    // projectLink.innerHTML = `<a href=/projects/${id}> ${projectName}</a>`
+    // projectLink.setAttribute('class', 'project__link')
+    // projectLink.setAttribute('id', `project-id-${id}`)
+    // projectList.appendChild(projectLink)
 });
