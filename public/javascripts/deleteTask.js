e87a784979803b18c11bdf3e6e2453195633a7f6
Vernyoon Chao <vernchao510@gmail.com>
2022-04-14 15:19:16 -0700

    deleteTask

--- public/javascripts/deleteTask.js
+++ public/javascripts/deleteTask.js
@@ -1,3 +1,27 @@
 window.addEventListener("load", (event) => {
   console.log("hello from deleteTask.js");
 });
+
+const deleteButtons = document.querySelectorAll(".delete__task");
+console.log(deleteButtons);
+
+async function deleteTask(e) {
+  e.stopPropagation();
+  const taskId = e.target.id.split("__")[2];
+
+  if (taskId) {
+    const res = await fetch(`/api/tasks/${taskId}`, {
+      method: "DELETE",
+    });
+    const data = await res.json();
+    if (data.message) {
+      const div = document.getElementById(`task-container-${task.id}`);
+      div.remove();
+    }
+  }
+}
+
+for (let i = 0; i < deleteButtons.length; i++) {
+  const deleteButton = deleteButtons[i];
+  deleteButton.addEventListener("click", deleteTask);
+}
