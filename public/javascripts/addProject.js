9d034a98be57a207ba8cab044b8d09bc1dfae799
Chris Threadgill <christhreadgill@live.com>
2022-04-14 15:27:29 -0500

    fixing edit-add-merge

--- public/javascripts/addProject.js
+++ public/javascripts/addProject.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", (event) => {
-    console.log("hello from javascript!");
+  console.log("hello from javascript!");
 });
 
 import { deleteProject } from "./deleteProject.js";
@@ -7,83 +7,83 @@ import { deleteProject } from "./deleteProject.js";
 const contentTypeJson = { "Content-Type": "application/json" };
 
 const toggleHidden = (e) => {
-    e.preventDefault();
-    document.getElementById("projectNameAdd").value = "";
-    document.getElementById("descriptionAdd").value = "";
-    document.getElementById("dueDateAdd").value = "";
-    document.getElementById("urlAdd").value = "";
-    document.getElementById("projectTypeAdd").value = "personal";
-    const addForm = document.getElementById("addProject");
+  e.preventDefault();
+  document.getElementById("projectNameAdd").value = "";
+  document.getElementById("descriptionAdd").value = "";
+  document.getElementById("dueDateAdd").value = "";
+  document.getElementById("urlAdd").value = "";
+  document.getElementById("projectTypeAdd").value = "personal";
+  const addForm = document.getElementById("addProject");
 
-    addForm.classList.toggle("hidden");
+  addForm.classList.toggle("hidden");
 };
 
 const addProject = document.getElementById("project__add__button");
 addProject.addEventListener("click", toggleHidden);
 
-const cancelProject = document.getElementById("cancel-add__Project__Button");
+const cancelProject = document.getElementById("cancel-add__project__button");
 cancelProject.addEventListener("click", toggleHidden);
 
 // todo add form values
-const newProject = document.getElementById("confirm__Project__Button");
-newProject.addEventListener("click", async(e) => {
-    e.preventDefault();
+const newProject = document.getElementById("confirm__project__button");
+console.log(newProject);
+newProject.addEventListener("click", async (e) => {
+  e.preventDefault();
 
-    const projectName = document.getElementById("projectNameAdd").value;
-    const description = document.getElementById("descriptionAdd").value;
-    const dueDate = document.getElementById("dueDateAdd").value;
-    const url = document.getElementById("urlAdd").value;
-    const projectType = document.getElementById("projectTypeAdd").value;
-    const _csrf = document.getElementById("csurfId").value;
+  const projectName = document.getElementById("projectNameAdd").value;
+  const description = document.getElementById("descriptionAdd").value;
+  const dueDate = document.getElementById("dueDateAdd").value;
+  const url = document.getElementById("urlAdd").value;
+  const projectType = document.getElementById("projectTypeAdd").value;
+  const _csrf = document.getElementById("csurfId").value;
 
-    const info = {
-        projectName,
-        description,
-        dueDate,
-        url,
-        projectType,
-        _csrf,
-    };
+  const info = {
+    projectName,
+    description,
+    dueDate,
+    url,
+    projectType,
+    _csrf,
+  };
+  console.log(info);
+  const newProject = await fetch("/api/projects", {
+    method: "post",
+    body: JSON.stringify(info),
+    headers: contentTypeJson,
+  });
 
-    const newProject = await fetch("/api/projects", {
-        method: "post",
-        body: JSON.stringify(info),
-        headers: contentTypeJson,
-    });
+  const project = await newProject.json();
 
-    const project = await newProject.json();
+  const { id } = project.project;
 
-    const { id } = project.project;
+  const projectList = document.querySelector(".project__list");
 
-    const projectList = document.querySelector(".project__list");
+  const projectLink = document.createElement("div");
 
-    const projectLink = document.createElement("div");
+  console.log(projectName);
+  console.log(id);
 
-    console.log(projectName);
-    console.log(id);
-
-    projectLink.innerHTML = `
-  <p class=project__List__Name id=project__List__Name${id}>
+  projectLink.innerHTML = `
+  <p class=project__list__name id=project__list__name${id}>
   ${projectName}
   </p>
   <span>
-  <button class=delete__Project id=delete__Project__${id}>
+  <button class=delete__project id=delete__project__${id}>
   <i class="fa-solid fa-trash-can" id=trash__icon__${id}>
   </i>
   </button>
   </span>
   `;
 
-    projectLink.setAttribute("class", "project__link");
-    projectLink.setAttribute("id", `project-id-${id}`);
-    projectList.appendChild(projectLink)
+  projectLink.setAttribute("class", "project__link");
+  projectLink.setAttribute("id", `project__id__${id}`);
+  projectList.appendChild(projectLink);
 
-    const newDeleteButton = document.getElementById(`delete__Project__${id}`);
-    const trashcans = document.querySelectorAll(".fa-trash-can");
-    for (let i = 0; i < trashcans.length; i++) {
-        let trashcan = trashcans[i];
-        trashcan.addEventListener("click", deleteProject);
-    }
-    newDeleteButton.addEventListener("click", deleteProject);
-
+  const newDeleteButton = document.getElementById(`delete__project__${id}`);
+  const trashcans = document.querySelectorAll(".fa-trash-can");
+  for (let i = 0; i < trashcans.length; i++) {
+    let trashcan = trashcans[i];
+    trashcan.addEventListener("click", deleteProject);
+  }
+  newDeleteButton.addEventListener("click", deleteProject);
 });
--- public/javascripts/deleteProject.js
+++ public/javascripts/deleteProject.js
@@ -1,30 +1,30 @@
 window.addEventListener("load", (event) => {
-    console.log("hello from deleteProject.js");
+  console.log("hello from deleteProject.js");
 });
 
-export const deleteProject = async(e) => {
-    e.stopPropagation()
-    const projectId = e.target.id.split("__")[2];
-    if (projectId) {
-        const res = await fetch(`/api/projects/${projectId}`, {
-            method: "DELETE",
-        });
-        const data = await res.json();
-        if (data.message) {
-            const div = document.getElementById(`project-id-${projectId}`);
-            div.remove();
-        }
+export const deleteProject = async (e) => {
+  e.stopPropagation();
+  const projectId = e.target.id.split("__")[2];
+  if (projectId) {
+    const res = await fetch(`/api/projects/${projectId}`, {
+      method: "DELETE",
+    });
+    const data = await res.json();
+    if (data.message) {
+      const div = document.getElementById(`project__id__${projectId}`);
+      div.remove();
     }
+  }
 };
 
-const deleteButtons = document.querySelectorAll(".delete__Project");
+const deleteButtons = document.querySelectorAll(".delete__project");
 for (let i = 0; i < deleteButtons.length; i++) {
-    const deleteBtn = deleteButtons[i];
-    deleteBtn.addEventListener("click", deleteProject);
+  const deleteBtn = deleteButtons[i];
+  deleteBtn.addEventListener("click", deleteProject);
 }
 
 const trashcans = document.querySelectorAll(".fa-trash-can");
 for (let i = 0; i < trashcans.length; i++) {
-    let trashcan = trashcans[i];
-    trashcan.addEventListener("click", deleteProject);
+  let trashcan = trashcans[i];
+  trashcan.addEventListener("click", deleteProject);
 }
