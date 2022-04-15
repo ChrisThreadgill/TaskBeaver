56b59660b4e02c772a377fed997469fd3168d522
Chris Threadgill <christhreadgill@live.com>
2022-04-15 15:19:59 -0500

    show project details button functionality is complete

---
+++ public/javascripts/projectDetails.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", (e) => {
+  console.log("hello from project details script");
+});
+
+const projectDetailsButtons = document.querySelectorAll(
+  ".project__details__button"
+);
+
+for (let i = 0; i < projectDetailsButtons.length; i++) {
+  const currentProjectDetailsButton = projectDetailsButtons[i];
+
+  const currentProjectId = currentProjectDetailsButton.id.split("__")[3];
+
+  const currentProjectDetails = document.getElementById(
+    `project__details__${currentProjectId}`
+  );
+
+  currentProjectDetailsButton.addEventListener("click", async (e) => {
+    currentProjectDetails.classList.toggle("hidden");
+  });
+}
